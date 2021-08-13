9449fc0634db1deff3278d0c7a4302153d3bdad7
zac <isaacmubiru99@gmail.com>
2021-08-13 15:00:30 +0300

    fix(route): change login route to use post rather than get

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,6 @@ const { signup, login } = require("../controllers/userController");
 const router = express.Router();
 
 router.post("/signup", signup);
-router.get("/login",login);
+router.post("/login",login);
 
 module.exports = router;
